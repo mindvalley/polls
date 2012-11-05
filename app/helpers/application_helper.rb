5c3f28577a748e3039005ff85c2948bf612bec5e
Tristan <tristan.gomez@gmail.com>
2012-11-06 03:00:53 +0800

    hardcode to 12-09

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def current_period
-    date = Date.today << 1
-    "#{date.strftime("%Y-%m")}"
+    # date = Date.today << 1
+    # "#{date.strftime("%Y-%m")}"
+    '2012-09'
   end
 end
